098bf97c511f91810a9c632917a0cb115ce77855
Anderson Enríquez <anderson.enriquez@correounivalle.edu.co>
2014-11-16 16:09:21 -0500

    proyecto reinicializado

---
+++ proyectoClinica/src/ejecutable/Ejecutar.java
@@ -0,0 +1,12 @@
+package ejecutable;
+
+import Vista.VentanaLogin;
+
+public class Ejecutar {
+    
+    public static void main (String [] args) {
+        
+        VentanaLogin iniciarSesion = new VentanaLogin();
+        
+    }
+}
