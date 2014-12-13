98241014305f27778f6f31c7b283cb77dc807897
Kazemu <mariano7@hotmail.es>
2014-12-13 14:46:51 -0500

    Agregados DriverMedicamento y DaoMedicamento

---
+++ proyectoClinica/src/controlador/DriverMedicamento.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import logica.Medicamento;
+import dataAccesss.DaoMedicamento;
+/**
+ *
+ * @author Mariano
+ */
+public class DriverMedicamento {
+    private DaoMedicamento daoMedicamento;
+
+    public DriverMedicamento() {
+        
+        daoMedicamento = new DaoMedicamento();
+        
+    }
+    
+    public void registrarMedicamento () {
+        
+        
+    }
+}
